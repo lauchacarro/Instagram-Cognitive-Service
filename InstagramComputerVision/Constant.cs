b60e11fe37936d1aa6e972262292b3c9cca35d77
lautaro <lautaroecarro@gmail.com>
2020-05-03 17:37:44 -0300

    Initial Commit: Only Instagram Service

---
+++ InstagramComputerVision/Constant.cs
@@ -0,0 +1,11 @@
+﻿namespace InstagramComputerVision
+{
+    public static class Constant
+    {
+        public const string INSTAUSERNAME = "InstagramUsername";
+        public const string INSTAPASSWORD = "InstagramPassword";
+        public const string COMPUTERVISIONKEY = "ComputerVisionKey";
+        public const string COMPUTERVISIONENDPOINT = "ComputerVisionEndpoint";
+        public const string INSTAPOST = "InstaPost";
+    }
+}
---
+++ InstagramComputerVision/Services/ComputerVisionService.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Blazored.LocalStorage;
+
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace InstagramComputerVision.Services
+{
+    public class ComputerVisionService
+    {
+        private readonly ILocalStorageService _localStorageService;
+
+        public ComputerVisionService(ILocalStorageService localStorageService)
+        {
+            _localStorageService = localStorageService;
+
+        }
+
+        public async Task AnalyzeImageUrl(string imageUrl)
+        {
+            string computerVisionKey = await _localStorageService.GetItemAsync<string>(Constant.COMPUTERVISIONKEY);
+            string endpointVisionKey = await _localStorageService.GetItemAsync<string>(Constant.COMPUTERVISIONENDPOINT);
+
+            ComputerVisionClient client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(computerVisionKey))
+            { Endpoint = endpointVisionKey };
+
+            // Creating a list that defines the features to be extracted from the image. 
+            List<VisualFeatureTypes> features = new List<VisualFeatureTypes>()
+        {
+          VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+          VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+          VisualFeatureTypes.Tags, VisualFeatureTypes.Adult,
+          VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
+          VisualFeatureTypes.Objects
+        };
+
+            ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, features);
+
+            Console.WriteLine("Summary:");
+            foreach (var caption in results.Description.Captions)
+            {
+                Console.WriteLine($"{caption.Text} with confidence {caption.Confidence}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Categories:");
+            foreach (var category in results.Categories)
+            {
+                Console.WriteLine($"{category.Name} with confidence {category.Score}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Tags:");
+            foreach (var tag in results.Tags)
+            {
+                Console.WriteLine($"{tag.Name} {tag.Confidence}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Objects:");
+            foreach (var obj in results.Objects)
+            {
+                Console.WriteLine($"{obj.ObjectProperty} with confidence {obj.Confidence} at location {obj.Rectangle.X}, " +
+                  $"{obj.Rectangle.X + obj.Rectangle.W}, {obj.Rectangle.Y}, {obj.Rectangle.Y + obj.Rectangle.H}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Faces:");
+            foreach (var face in results.Faces)
+            {
+                Console.WriteLine($"A {face.Gender} of age {face.Age} at location {face.FaceRectangle.Left}, " +
+                  $"{face.FaceRectangle.Left}, {face.FaceRectangle.Top + face.FaceRectangle.Width}, " +
+                  $"{face.FaceRectangle.Top + face.FaceRectangle.Height}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Adult:");
+            Console.WriteLine($"Has adult content: {results.Adult.IsAdultContent} with confidence {results.Adult.AdultScore}");
+            Console.WriteLine($"Has racy content: {results.Adult.IsRacyContent} with confidence {results.Adult.RacyScore}");
+            Console.WriteLine();
+
+            Console.WriteLine("Brands:");
+            foreach (var brand in results.Brands)
+            {
+                Console.WriteLine($"Logo of {brand.Name} with confidence {brand.Confidence} at location {brand.Rectangle.X}, " +
+                  $"{brand.Rectangle.X + brand.Rectangle.W}, {brand.Rectangle.Y}, {brand.Rectangle.Y + brand.Rectangle.H}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Celebrities:");
+            foreach (var category in results.Categories)
+            {
+                if (category.Detail?.Celebrities != null)
+                {
+                    foreach (var celeb in category.Detail.Celebrities)
+                    {
+                        Console.WriteLine($"{celeb.Name} with confidence {celeb.Confidence} at location {celeb.FaceRectangle.Left}, " +
+                          $"{celeb.FaceRectangle.Top}, {celeb.FaceRectangle.Height}, {celeb.FaceRectangle.Width}");
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Landmarks:");
+            foreach (var category in results.Categories)
+            {
+                if (category.Detail?.Landmarks != null)
+                {
+                    foreach (var landmark in category.Detail.Landmarks)
+                    {
+                        Console.WriteLine($"{landmark.Name} with confidence {landmark.Confidence}");
+                    }
+                }
+            }
+            Console.WriteLine();
+            // </snippet_landmarks>
+
+            // <snippet_color>
+            // Identifies the color scheme.
+            Console.WriteLine("Color Scheme:");
+            Console.WriteLine("Is black and white?: " + results.Color.IsBWImg);
+            Console.WriteLine("Accent color: " + results.Color.AccentColor);
+            Console.WriteLine("Dominant background color: " + results.Color.DominantColorBackground);
+            Console.WriteLine("Dominant foreground color: " + results.Color.DominantColorForeground);
+            Console.WriteLine("Dominant colors: " + string.Join(",", results.Color.DominantColors));
+            Console.WriteLine();
+            // </snippet_color>
+
+            // <snippet_type>
+            // Detects the image types.
+            Console.WriteLine("Image Type:");
+            Console.WriteLine("Clip Art Type: " + results.ImageType.ClipArtType);
+            Console.WriteLine("Line Drawing Type: " + results.ImageType.LineDrawingType);
+            Console.WriteLine();
+            // </snippet_type>
+        }
+    }
+}
