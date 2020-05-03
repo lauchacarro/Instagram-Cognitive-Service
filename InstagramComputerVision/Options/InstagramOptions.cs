877847ee5fbade33311a23b05713f1ed5da4a21b
lautaro <lautaroecarro@gmail.com>
2020-05-03 19:29:39 -0300

    add: InstagramOptions

---
+++ InstagramComputerVision/Options/InstagramOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace InstagramComputerVision.Options
+{
+    public class InstagramOptions
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
